c6ecbc2e2d31278ea2cfd877170414aa1f9f5e5f
rojasoscar752 <rojasoscar752@gmail.com>
2025-04-06 20:50:40 -0500

    estructura parcial2

---
+++ src/src/main/java/model/Paciente.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Paciente {
+    private String numDoc;
+    private String namePac;
+
+    public String getNumDoc() {
+        return numDoc;
+    }
+
+    public void setNumDoc(String numDoc) {
+        this.numDoc = numDoc;
+    }
+
+    public String getNamePac() {
+        return namePac;
+    }
+
+    public void setNamePac(String namePac) {
+        this.namePac = namePac;
+    }
+}
