65f0d364f6c3956976f4445fb37c9776597a5334
leo <miguel.avila04@uptc.edu.co>
2025-04-06 21:40:35 -0500

    Agregar prototype y singleton

---
+++ src/src/main/java/model/SystemSettings.java
@@ -0,0 +1,10 @@
+package model;
+
+public class SystemSettings {
+    private SystemSettings instance;
+    private String language;
+
+
+    private SystemSettings(){};
+    public instance getInstance(); //aca deberia de ir SystemSetting en vez de instance
+}
